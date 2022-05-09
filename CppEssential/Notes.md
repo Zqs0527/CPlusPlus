369e4eced9b87509455a13e134ceb1d93db888cb
Yuan Chen <ccyyzqs@gmail.com>
2022-05-09 22:58:30 +0200

    Details about the data type

--- CppEssential/Notes.md
+++ CppEssential/Notes.md
@@ -39,3 +39,92 @@ y = *ip; // this statements copies the value pointed to by ip into y. this aster
 - No need to use dereference (* asterisk). The value of a reference is always the referred value
 - There is no syntax for changing the reference. Once it is defined, it can't be changed
 - Recommend that reference has const as a modifier
+
+### Primitive array
+
+```
+int ia[5]; <-> int * ip = ia; // initialize an integer array, the pointer is pointing to the first element of the array
+*ip = 2;
+ia[0] = 1; <-> *ia = 1;
+
+++ip; // move the pointer to the address of the next element. pointer knows the size of the element, this increment will be sure to point to the next element
+*ip = 3; // this will assign the value to the second element
+
+*(++ip) = 4; // assign the value to the third element
+```
+
+### Primitives string
+```
+char s[] = "string"; <-> char s[] = {'s','t','r','i','n','g',0};
+```
+
+### Range for loop
+```
+for (int x: arrayInput){
+    printf("%d\n", x);
+}
+```
+
+### cout
+- cout is an STL class. STL is the standard template library. Introduce STL, which requires CPU and space. And it might introduce more errors
+- One advantage is that cout doesn't care about the data type
+
+## Chapter 3
+### Data types
+
+### Characters and strings
+```
+char cstring[] = "String"; // it copies literal string to the array
+const char * cstring = "String"; // makes the pointer to point to the type of the pointer. This is not a constant pointer. It is not copying the the literal string to an array
+```
+
+### Character escape
+- backslash `\`t
+
+### Qualifier
+`const static int- i = 42; // const and static are qualifiers`
+
+- Constant and Volatile qualifiers (CV Qualifiers)
+    - `const`
+    - `mutable`
+    - `voltile`; is deprecated
+- Storage Duration
+    - `static`: if a variable is declared as static, there is a static memory to for that variable
+    - `register`
+    - `extern`
+
+### References
+Common usage
+```
+int & f(int & i){
+    return ++i;
+}
+
+int main(){
+    int i = 5;
+    printf("f(i) is %d\n", f(i));
+}
+```
+Best practice is to return a `const` reference
+```
+const int & f(const int & i){
+    return i;
+}
+```
+
+### Structure
+```
+struct Employee = {
+    int id;
+    const char * name;
+    const char * role;
+}
+
+int main(){
+    Employee employee = {2, "Datre", "Engineer"};
+    Employee * j = &employee;
+    j->name;
+    j->role;
+    j->id;
+}
+```
--- CppEssential/main.cpp
+++ CppEssential/main.cpp
@@ -1,26 +1,126 @@
-#include <iostream>
 #include <cstdio>
 
-int main(int, char**) {
-    int x = 42;
-    const int & y = x;
-    int * ip = &x;
+void func() { puts("this is func"); }
 
-    printf("the value of x is %d\n", x);
-    printf("the value of y is %d\n", y);
-    printf("the value of *ip is %d\n", *ip);
+class SimpleClass {
+private:
+  int i = 0;
 
-   
-    printf("the value of x is %d\n", x);
-    printf("the value of y is %d\n", y);
-    printf("the value of *ip is %d\n", *ip);
+public:
+  void setValue(int value) { i = value; };
+  int getValue() { return i; };
+};
 
-    int z = 10;
-    ip = &z;
-    printf("the value of *ip is %d\n", *ip);
+const char *inputString = "This is a null-terminated string.";
 
+class CheckQualifier {
+public:
+  int static_value() {
+    static int x = 7; // this static integer in this class has a global storage
+    return ++x;
+  }
+};
 
-    // x = 6;
-    // printf("the value of *ip is %d\n", *ip);
-    puts("Hello, world!\n");
+int functionSimple() {
+  static int x = 7;
+  return ++x;
+};
+
+int &f(int &i) { return ++i; };
+
+int main(int, char **) {
+  int inputNumber = 5;
+  f(inputNumber) = 43;;
+  printf("f(i) is %d\n", f(inputNumber));
+  printf("i is %d\n", inputNumber);
+
+  puts("Hello world. \' \" \x40 \u03bc");
+  CheckQualifier cq1;
+  CheckQualifier cq2;
+  CheckQualifier cq3;
+  printf("The value of i is %d\n", cq1.static_value());
+  printf("The value of i is %d\n", cq2.static_value());
+  printf("The value of i is %d\n", cq3.static_value());
+
+  // int testValue = functionSimple();
+  // testValue = functionSimple();
+  // testValue = functionSimple();
+  // printf("The value of i is %d\n", testValue);
+
+  float f;
+  double d;
+  long double ld;
+  printf("size of float is %zd bits\n", sizeof(f) * 8);
+  printf("size of double is %zd bits\n", sizeof(d) * 8);
+  printf("size of long double is %zd bits\n", sizeof(ld) * 8);
+
+  char c = 0;
+  short si = 0;
+  int i = 0;
+  long int li = 0;
+  long long int lli = 0;
+  printf("size of char is %zd bits\n", sizeof(c) * 8);
+  printf("size of short int is %zd bits\n", sizeof(si) * 8);
+  printf("size of int is %zd bits\n", sizeof(i) * 8);
+
+  printf("size of long int is %zd bits\n", sizeof(li) * 8);
+  printf("size of long long int is %zd bits\n", sizeof(lli) * 8);
+
+  int count = 0;
+  for (const char *cp = inputString; *cp; ++cp) {
+    count++;
+  }
+  printf("The number of characters is: %d\n", count);
+
+  puts("this is main");
+  int inputValue = 9;
+  SimpleClass simpleClass;
+  simpleClass.setValue(inputValue);
+  printf("value is %d\n", simpleClass.getValue());
+
+  func();
+  struct structure {
+    int i;
+    double d;
+    const char *s;
+  };
+
+  structure str = {1, 1.24, "this a string"};
+
+  char s[] = {'s', 't', 'r', 'i', 'n', 'g', 0};
+  printf("s is %s\n", s);
+
+  // for (int i = 0; s[i] != 0; i++)
+  // {
+  //     printf("%c\n", s[i]);
+  // }
+  for (char *cp = s; *cp; ++cp) {
+    printf("%c\n", *cp);
+  }
+
+  int arrayInit[] = {1, 2, 3, 4};
+  for (int &x : arrayInit) {
+    printf("%d ", x);
+  }
+  puts("\n");
+
+  int x = 42;
+  const int &y = x;
+  int *ip = &x;
+
+  printf("the value of x is %d\n", x);
+  printf("the value of y is %d\n", y);
+  printf("the value of *ip is %d\n", *ip);
+
+  printf("the value of x is %d\n", x);
+  printf("the value of y is %d\n", y);
+  printf("the value of *ip is %d\n", *ip);
+
+  int z = 10;
+  ip = &z;
+  printf("the value of *ip is %d\n", *ip);
+
+  // x = 6;
+  // printf("the value of *ip is %d\n", *ip);
+  puts("Hello, world!\n");
 }
