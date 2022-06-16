6468610feac85727f839df843d6ce72a95b46f72
Yuan Chen <ccyyzqs@gmail.com>
2022-06-16 22:36:43 +0200

    More implementation on weather statistics

--- CppCodeClinic/Ch01/WeatherStatistics.cpp
+++ CppCodeClinic/Ch01/WeatherStatistics.cpp
@@ -7,6 +7,8 @@
 #include <string>
 #include <filesystem>
 #include <sstream>
+#include <ctime>
+#include <map>
 
 using namespace std;
 
@@ -33,32 +35,67 @@ vector<string> GetCloumnValues(string &inputValue, const char deli){
     return outputStrings;
 }
 
+time_t ConvertTimeStamps(string dateInfo){
+  struct std::tm tm;
+  std::istringstream ss(dateInfo);
+  ss >> std::get_time(&tm, "%Y-%m-%d %H:%M:%S");
+
+  time_t t = mktime(&tm); 
+
+  return t;
+}
+
 int main() {
+
+  // Start date and time
+  string startDate, startTime;
+  cout << "Please endter the start date, format is yyyy-mm-dd: ";
+  cin >> startDate;
+  cout << "Please endter the start time, format is hh-mm-ss (24 hours): ";
+  cin  >> startTime;
+  // End date and time
+  string endDate, endTime;
+  cout << "Please endter the end date, format is yyyy-mm-dd: ";
+  cin >> endDate;
+  cout << "Please endter the end time, format is hh-mm-ss (24 hours): ";
+  cin  >> endTime;
+
   const char *dirName = "C:\\Users\\yuanc\\Repository\\CPlusPlus\\CppCodeClinic\\Ch01\\resources";
 
   vector<std::filesystem::directory_entry> collectionFiles = ReadDirectory(dirName);
   
   ifstream inputFile;
+  
   inputFile.open(collectionFiles[0].path());
 
   string line;
   const char delimiter = '\t';
   if (inputFile.fail()) {
-    const char *errorMessage = strerror(errno);
+    const char *errorMessage = strerror(errno); 
     printf("There is an error to open the file %s, code %d: %s", collectionFiles[0].path(),
            errno, errorMessage);
            return 1;
   }
   // Get the header of the file
   getline(inputFile, line);
-  
+  map<time_t,double> timeToPressure;
+  char *p;
   while (!inputFile.eof()) {
     getline(inputFile, line, '\n');
-    
-    vector<string> segments = GetCloumnValues(line, delimiter);
     if (line == "") {
       break;
     }
+    vector<string> segments = GetCloumnValues(line, delimiter);
+    try
+    {
+       time_t timeAfterConversion = ConvertTimeStamps(segments[0]);
+       timeToPressure[timeAfterConversion] = atof(segments[2].c_str());
+    }
+    catch(const std::exception& e)
+    {
+      std::cerr << e.what() << '\n';
+    }
+    
   }
 
   inputFile.close();
